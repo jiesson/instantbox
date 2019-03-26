03d370abe1c8d6635023d33de450ce071329482d
jiesson <45846029+jiesson@users.noreply.github.com>
2019-03-26 22:10:03 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,25 +8,27 @@
 #  bash <(curl -sSL https://raw.githubusercontent.com/instantbox/instantbox/master/init.sh)"
 #  docker-compose up -d
 #
-
+#检查命令位置，$1为命令参数
 check_cmd() {
     command -v "$1" >/dev/null 2>&1
 }
 
 echo "Welcome to instantbox, please wait..."
 echo ""
-
+#是否存在docker命令
 if check_cmd docker; then
     echo "docker is installed"
 else
     echo "docker is not installed, please try again after it's installed"
     exit 1
 fi
-
+#同理，检查docker-compose
 if check_cmd docker-compose; then
     echo "docker-compose is installed"
 else
+#若未安装docker-compose，那就给你安一个
     curl -sSL https://raw.githubusercontent.com/docker/compose/master/script/run/run.sh > /usr/local/bin/docker-compose
+#给执行权限，失败就返回1
     chmod +x /usr/local/bin/docker-compose || exit 1
 fi
 
